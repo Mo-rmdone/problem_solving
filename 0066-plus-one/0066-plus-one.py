4f1a37139890821eb23436fd10d75c37fde7ed12
Mohamed  Ramadan <74499373+Mo-rmdone@users.noreply.github.com>
2024-06-02 08:20:02 +0300

    Time: 40 ms (28.81%), Space: 16.5 MB (80.84%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,19 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-        
-        # Start from the end of the array
-        for i in range(n-1, -1, -1):
-            # Add one to the current digit
-            digits[i] += 1
-            
-            # If the current digit is less than 10, we're done
-            if digits[i] < 10:
-                return digits
-            
-            # Otherwise, set the current digit to 0 and continue
-            digits[i] = 0
-        
-        # If we're here, it means we had a carry out from the most significant digit
-        # So we need to add a new digit at the beginning
-        return [1] + digits
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+        return digits[::-1]
