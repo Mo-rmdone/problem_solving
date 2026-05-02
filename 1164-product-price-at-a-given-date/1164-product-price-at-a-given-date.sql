1b0fbefc4e3a765f89e5e710f2c760ffd7fb3539
Mohamed  Ramadan <74499373+Mo-rmdone@users.noreply.github.com>
2026-05-02 20:52:46 +0300

    Time: 760 ms (14%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,13 +1,13 @@
 SELECT 
     p.product_id, 
-    IFNULL(lp.new_price, 10) AS price
+    IFNULL(p2.new_price, 10) AS price
 FROM 
     (SELECT DISTINCT product_id FROM products) p
 LEFT JOIN 
-    products lp 
+    products p2 
 ON 
-    p.product_id = lp.product_id 
-    AND lp.change_date = (
+    p.product_id = p2.product_id 
+    AND p2.change_date = (
         SELECT MAX(change_date) 
         FROM products 
         WHERE product_id = p.product_id 
