4067a3f8b901a3d912b243a651f51e5471ff32c1
Mohamed  Ramadan <74499373+Mo-rmdone@users.noreply.github.com>
2026-06-24 02:58:47 +0300

    Time: 79 ms (59.58%), Space: 32.2 MB (23.11%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+      
+      
+        for index, cost in enumerate(costs):
+  
+            if coins < cost:
+                return index
+          
+            coins -= cost
+        return len(costs)
